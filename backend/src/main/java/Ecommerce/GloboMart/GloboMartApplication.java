7e67643433cd0ec507b4bfb7bc08340358278b9c
Nirodha Buddhima <nirodhabuddhima@gmail.com>
2024-01-07 17:58:29 +0530

    commits

---
+++ backend/src/main/java/Ecommerce/GloboMart/GloboMartApplication.java
@@ -0,0 +1,13 @@
+package Ecommerce.GloboMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloboMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloboMartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Ecommerce/GloboMart/GloboMartApplicationTests.java
@@ -0,0 +1,13 @@
+package Ecommerce.GloboMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloboMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
