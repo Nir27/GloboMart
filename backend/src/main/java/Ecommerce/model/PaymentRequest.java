907fbe2be341ac68c197f64d71ebe2dc9f103810
Nirodha Buddhima <nirodhabuddhima@gmail.com>
2024-04-06 00:33:35 +0530

    Add product, user and request models

---
+++ backend/src/main/java/Ecommerce/model/PaymentRequest.java
@@ -0,0 +1,11 @@
+
+public class PaymentRequest {
+	private String cardNumber;
+	private String cardHolderName;
+	private String expiryDate;
+	private int cvv;
+	private double amount;
+	// Other attributes like billing address, currency, etc.
+
+	// Getters and setters
+}
